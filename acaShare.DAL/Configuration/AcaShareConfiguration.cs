20a410851b5376d6bb9782ae8816605ad19f93e2
Maciej Sadoś <s14051@pjwstk.edu.pl>
2018-10-09 00:58:24 +0200

    Used 'User Secrets Manager' to store app's connection string and implemented usage example

---
+++ acaShare.DAL/Configuration/AcaShareConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace acaShare.DAL.Configuration
+{
+    public class AcaShareConfiguration
+    {
+        public string ConnectionString { get; set; }
+    }
+}
--- acaShare.DAL/Persistence/UnitOfWork.cs
+++ acaShare.DAL/Persistence/UnitOfWork.cs
@@ -1,13 +1,27 @@
-﻿using acaShare.DAL.Core;
+﻿using acaShare.DAL.Configuration;
+using acaShare.DAL.Core;
 using acaShare.DAL.Core.Repositories;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Text;
 
 namespace acaShare.DAL.Persistence
 {
     public class UnitOfWork : IUnitOfWork
     {
+        private readonly IDbConnection _con;
+
+        public UnitOfWork(IOptions<AcaShareConfiguration> options)
+        {
+            AcaShareConfiguration configuration = options.Value;
+            var connStr = configuration.ConnectionString;
+            _con = new SqlConnection(connStr);
+            _con.Open();
+        }
+
         public ILessonRepository Lessons { get; }
 
         public void SaveChanges()
