d6ac503b6932348b00dd05fda370a27432b23329
Maciej Sadoś <s14051@pjwstk.edu.pl>
2018-11-02 00:43:30 +0100

    Added moderator's actions and prepared a base view+controller for a structure management

---
+++ acaShare.MVC/Areas/Moderator/Controllers/StructureManagementController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace acaShare.MVC.Areas.Moderator.Controllers
+{
+    [Authorize]
+    [Area("Moderator")]
+    public class StructureManagementController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+    }
+}
