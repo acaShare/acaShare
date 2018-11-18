6f048f9b0a4cfd2a6d25cc0b38136ae2c634300a
s13814 <s13814@pjwstk.edu.pl>
2018-11-18 16:35:44 +0100

    Welcome page added. Login, register and page not found view updated.

---
+++ acaShare.MVC/Areas/Identity/Controllers/WelcomePageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace acaShare.MVC.Areas.Identity.Controllers
+{
+    [Area("Identity")]
+    public class WelcomePageController : Controller
+    {
+        public IActionResult WelcomePage()
+        {
+            return View("Areas/Identity/Pages/Account/WelcomePage.cshtml");
+        }
+    }
+}
