af5160e97ccb9f3d38ea5c225c4e0840a8443058
Maciej Sadoś <s14051@pjwstk.edu.pl>
2018-12-20 01:17:16 +0100

    Added displaying active links in moderator panel

--- acaShare.MVC/wwwroot/js/moderator-panel.js
+++
@@ -1,4 +0,0 @@
-﻿window.onload(() => {
-    let actionsContainer = document.getElementById('moderator-actions');
-    actionsContainer.childNodes.forEach(n => n.addEventListener("onclick", () => { n.classList.toggle('action-active'); }));
-});
