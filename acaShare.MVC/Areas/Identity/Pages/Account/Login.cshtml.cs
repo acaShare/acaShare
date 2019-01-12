f9eee57ead3389b98071a23327f841d912e6386f
Maciej Sadoś <s14051@pjwstk.edu.pl>
2019-01-12 15:59:28 +0100

    Disabled locking out accounts

--- acaShare.MVC/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ acaShare.MVC/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -75,7 +75,7 @@ namespace acaShare.MVC.Areas.Identity.Pages.Account
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.UserName, Input.Password, Input.RememberMe, lockoutOnFailure: true);
+                var result = await _signInManager.PasswordSignInAsync(Input.UserName, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
