d6c6279b8557af3436a741d7719a5439d8c996ea
s13814 <s13814@pjwstk.edu.pl>
2019-01-20 18:36:27 +0100

    Now main moderator can only manage structure of university that he is assigned to.

--- acaShare.MVC/Areas/Moderator/Controllers/StructureManagement/UniversitiesManagementController.cs
+++ acaShare.MVC/Areas/Moderator/Controllers/StructureManagement/UniversitiesManagementController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using acaShare.MVC.Common;
 using acaShare.MVC.Models;
@@ -19,14 +20,18 @@ namespace acaShare.MVC.Areas.Moderator.Controllers.StructureManagement
         private readonly IUniversityTreeManagementService _managementService;
         private readonly IMaterialsService _materialsService;
         private readonly IFormFilesManagement _filesManagement;
+        private readonly IUserService _userservice;
+        private readonly IMainModeratorService _mainModeratorService;
 
         public UniversitiesManagementController(IUniversityTreeTraversalService traversalService, IUniversityTreeManagementService managementService,
-            IMaterialsService materialsService, IFormFilesManagement formFilesManagement)
+            IMaterialsService materialsService, IFormFilesManagement formFilesManagement, IUserService userService, IMainModeratorService mainModeratorService)
         {
             _traversalService = traversalService;
             _managementService = managementService;
             _materialsService = materialsService;
             _filesManagement = formFilesManagement;
+            _userservice = userService;
+            _mainModeratorService = mainModeratorService;
         }
 
 
@@ -34,34 +39,62 @@ namespace acaShare.MVC.Areas.Moderator.Controllers.StructureManagement
         {
             ConfigureListBreadcrumbs();
 
-            var universities = _traversalService.GetUniversities();
-
-            var universityViewModels = universities.Select(u =>
-                new UniversityViewModel
-                {
-                    Id = u.UniversityId,
-                    TitleOrFullName = u.Name,
-                    SubtitleOrAbbreviation = u.Abbreviation,
-                }
-            ).ToList();
-            
-            var vm = new ListViewModel<UniversityViewModel>
+            if (User.IsInRole(Roles.MainModeratorRole))
             {
-                Items = universityViewModels,
-                IsWithSubtitles = true
-            };
+                var identityUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+                var appUser = _userservice.FindByIdentityUserId(identityUserId);
+                var mainModeratorUniversity = _mainModeratorService.GetUniversityMainModerator(appUser.UserId);
+                var university = _traversalService.GetUniversity(mainModeratorUniversity.UniversityId);
 
-            return View(vm);
+                var universityViewModel = new List<UniversityViewModel>
+                {
+                    new UniversityViewModel
+                    {
+                        Id = university.UniversityId,
+                        TitleOrFullName = university.Name,
+                        SubtitleOrAbbreviation = university.Abbreviation
+                    }
+                };
+
+                var vm = new ListViewModel<UniversityViewModel>
+                {
+                    Items = universityViewModel,
+                    IsWithSubtitles = true
+                };
+
+                return View(vm);
+            }
+            else
+            {
+                var universities = _traversalService.GetUniversities();
+
+                var universityViewModels = universities.Select(u =>
+                    new UniversityViewModel
+                    {
+                        Id = u.UniversityId,
+                        TitleOrFullName = u.Name,
+                        SubtitleOrAbbreviation = u.Abbreviation,
+                    }
+                ).ToList();
+
+                var vm = new ListViewModel<UniversityViewModel>
+                {
+                    Items = universityViewModels,
+                    IsWithSubtitles = true
+                };
+
+                return View(vm);
+            }
         }
 
-
+        [Authorize(Roles = Roles.AdministratorRole)]
         public IActionResult Add()
         {
             ConfigureAddBreadcrumbs();
             return View();
         }
 
-
+        [Authorize(Roles = Roles.AdministratorRole)]
         [HttpPost]
         public IActionResult Add(UniversityViewModel vm)
         {
@@ -83,7 +116,7 @@ namespace acaShare.MVC.Areas.Moderator.Controllers.StructureManagement
             return RedirectToAction("Universities");
         }
 
-
+        [Authorize(Roles = Roles.AdministratorRole)]
         public IActionResult Edit(int universityId)
         {
             var universityToEdit = _traversalService.GetUniversity(universityId);
@@ -104,7 +137,7 @@ namespace acaShare.MVC.Areas.Moderator.Controllers.StructureManagement
             return View(vm);
         }
 
-
+        [Authorize(Roles = Roles.AdministratorRole)]
         [HttpPost]
         public IActionResult Edit(UniversityViewModel vm)
         {
@@ -132,7 +165,7 @@ namespace acaShare.MVC.Areas.Moderator.Controllers.StructureManagement
             return RedirectToAction("Universities");
         }
 
-
+        [Authorize(Roles = Roles.AdministratorRole)]
         public IActionResult Delete(int universityId, bool confirmation = false)
         {
             var universityToDelete = _traversalService.GetUniversity(universityId);
