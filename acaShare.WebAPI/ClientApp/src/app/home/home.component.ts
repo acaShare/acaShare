29a588a936dc0605aec8791e400f66149e1ec05c
Maciej Sadoś <rubik@pjwstk.edu.pl>
2020-03-12 17:55:54 +0100

    Added test api query and fundamentals of shared layout

--- acaShare.WebAPI/ClientApp/src/app/home/home.component.ts
+++ acaShare.WebAPI/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { University } from './university';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  apiUrl = "api/v1/universitiesManagement";
+  universities: University[];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http
+      .get<University[]>(this.apiUrl)
+      .subscribe(universities => this.universities = universities, error => console.log(error));
+  }  
 }
